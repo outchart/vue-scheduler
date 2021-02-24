534112b9f8e153148fb733309df468f8ff17e112
Alexey Chernov <shadmas@gmail.com>
2021-02-24 13:49:28 +0300

    hourIndex+1

--- src/components/Scheduler.vue
+++ src/components/Scheduler.vue
@@ -82,10 +82,10 @@
           :class="{
             'scheduler-active': isCellSelected(hourIndex, dayIndex)
           }"
-          @mousedown="handleMouseDown(hourIndex - 1, dayIndex)"
-          @mousemove="handleMouseMove(hourIndex - 1, dayIndex)"
-          @touchstart="handleMouseDown(hourIndex - 1, dayIndex)"
-          @touchmove="handleMouseMove(hourIndex - 1, dayIndex)"
+          @mousedown="handleMouseDown(hourIndex + 1, dayIndex)"
+          @mousemove="handleMouseMove(hourIndex + 1, dayIndex)"
+          @touchstart="handleMouseDown(hourIndex + 1, dayIndex)"
+          @touchmove="handleMouseMove(hourIndex + 1, dayIndex)"
         />
       </tr>
     </tbody>
