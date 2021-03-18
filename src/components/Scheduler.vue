4bdd050343de90ec89ea0b67f3d80be93b18d75c
Alexey Chernov <shadmas@gmail.com>
2021-03-18 11:43:03 +0300

    ignoreWeekend

--- src/components/Scheduler.vue
+++ src/components/Scheduler.vue
@@ -291,7 +291,7 @@ export default {
       const fromColIndex = hour * this.accuracy
       const toColIndex = fromColIndex + this.accuracy - 1
       const startCoord = [fromColIndex, this.ignoreWeekend ? 1 : 1] // [row, col] row start form 1
-      const endCoord = [toColIndex, this.ignoreWeekend ? 5 : 6]
+      const endCoord = [toColIndex, this.ignoreWeekend ? 6 : 6]
       const selectMode = this.getRangeSelectMode(startCoord, endCoord)
       this.updateToggle(startCoord, endCoord, selectMode)
     },
